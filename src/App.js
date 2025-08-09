67ca3423c3b610dc9c6e89ccbb0b9f8187523f14
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-09 11:09:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
   return (
     <div>
       <NavBar />
+    {/* routes for different pages */}
       <Routes>
           <Route exact path="/" element={<Hero />} />
           <Route path="/about" element={<About />} />
