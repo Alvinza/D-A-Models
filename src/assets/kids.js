aca4d0fb2260996e8375cebf92b8b74a3be11a3e
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-09 12:00:17 +0200

    muted imports

--- src/assets/kids.js
+++ src/assets/kids.js
@@ -6,10 +6,10 @@ import kid5 from '../assets/kids5.png'
 import kid6 from '../assets/kids6.png'
 import kid7 from '../assets/girls9.png'
 import kid8 from '../assets/kids8.png'
-import kid9 from '../assets/kids9.png'
-import kid10 from '../assets/kids10.png'
-import kid11 from '../assets/kids11.png'
-import kid12 from '../assets/kids12.png'
+// import kid9 from '../assets/kids9.png'
+// import kid10 from '../assets/kids10.png'
+// import kid11 from '../assets/kids11.png'
+// import kid12 from '../assets/kids12.png'
 
 const kidsModel = [
     {
@@ -44,22 +44,8 @@ const kidsModel = [
         kidPicture: kid8,
         kidName: "JESUS G"
     },
-    {
-        kidPicture: kid9,
-        kidName: "GIULIO"
-    },
-    {
-        kidPicture: kid10,
-        kidName: "INI"
-    },
-    {
-        kidPicture: kid11,
-        kidName: "ZARI"
-    },
-    {
-        kidPicture: kid12,
-        kidName: "SIYA"
-    },
+  
+
 ]
 
 export default kidsModel;
