a58e5f7b5a0b46766b76b109a319e42fa37fdd89
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-09 10:36:10 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+// Get the current year
 const date = new Date();
 const currentYear = date.getFullYear();
 
 const Footer = () => {
+   // Footer container
   return (
     <div className='text-center p-6 mt-10' style={{backgroundColor: '#F1BC56'}}>
         <p className='text-gray-800'>`Copyright &copy; {currentYear} D&A Models | All Rights Reserved | Policy | Powered by Mainboard.`</p>
