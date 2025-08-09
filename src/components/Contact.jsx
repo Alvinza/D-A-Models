29686558ea84719b4f2cba833e8142967d47330e
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-09 10:32:51 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "./Title.jsx";
 import mapPic from "../assets/map.png";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // Navigation hook
 
 const Contact = () => {
   const navigate = useNavigate();
